dc35a241bf6f937627cc91f8923b70e13bd7b1f1
Kevin Sheehan <kshhn12@gmail.com>
2020-08-15 22:53:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,6 +7,7 @@ def game_hash()
       colors: ["Black", "White"],
       players: [
         "Alan Anderson" => {
+          player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 22,
@@ -17,6 +18,7 @@ def game_hash()
           slam_dunks: 1
         },
         "Reggie Evans" => {
+          player_name: "Bismack Biyombo",
           number: 30,
           shoe: 14,
           points: 12,
@@ -27,6 +29,7 @@ def game_hash()
           slam_dunks: 7
         },
         "Brook Lopez" => {
+          player_name: "Bismack Biyombo",
           number: 11,
           shoe: 17,
           points: 17,
@@ -37,6 +40,7 @@ def game_hash()
           slam_dunks: 15
         },
         "Mason Plumlee" => {
+          player_name: "Bismack Biyombo",
           number: 1,
           shoe: 19,
           points: 26,
@@ -47,6 +51,7 @@ def game_hash()
           slam_dunks: 5
         },
         "Jason Terry" => {
+          player_name: "Bismack Biyombo",
           number: 31,
           shoe: 15,
           points: 19,
@@ -63,7 +68,7 @@ def game_hash()
       colors: ["Turquoise", "Purple"],
       players: [
         "Jeff Adrien" => {
-          player_name: "Jeff Adrien"
+          player_name: "Jeff Adrien",
           number: 4,
           shoe: 18,
           points: 10,
@@ -74,7 +79,7 @@ def game_hash()
           slam_dunks: 2
         },
         "Bismack Biyombo" => {
-          player_name: "Bismack Biyombo"
+          player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
@@ -85,6 +90,7 @@ def game_hash()
           slam_dunks: 10
         },
         "DeSagna Diop" => {
+          player_name: "Bismack Biyombo",
           number: 2,
           shoe: 14,
           points: 24,
@@ -95,6 +101,7 @@ def game_hash()
           slam_dunks: 5
         },
         "Ben Gordon" => {
+          player_name: "Bismack Biyombo",
           number: 8,
           shoe: 15,
           points: 33,
@@ -105,6 +112,7 @@ def game_hash()
           slam_dunks: 0
         },
         "Kemba Walker" => {
+          player_name: "Bismack Biyombo",
           number: 33,
           shoe: 15,
           points: 6,
