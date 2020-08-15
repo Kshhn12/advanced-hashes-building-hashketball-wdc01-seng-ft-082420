f3d5f5f802471db0a86e318360f55ae14ae10f3f
Kevin Sheehan <kshhn12@gmail.com>
2020-08-15 22:57:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,8 +6,7 @@ def game_hash()
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
       players: [
-        "Alan Anderson" => {
-          player_name: "Alan Anderson",
+        player_name: "Alan Anderson" => {
           number: 0,
           shoe: 16,
           points: 22,
@@ -17,8 +16,7 @@ def game_hash()
           blocks: 1,
           slam_dunks: 1
         },
-        "Reggie Evans" => {
-          player_name: "Reggie Evans",
+        player_name: "Reggie Evans" => {
           number: 30,
           shoe: 14,
           points: 12,
@@ -28,8 +26,7 @@ def game_hash()
           blocks: 12,
           slam_dunks: 7
         },
-        "Brook Lopez" => {
-          player_name: "Brook Lopez",
+        player_name: "Brook Lopez" => {
           number: 11,
           shoe: 17,
           points: 17,
@@ -39,8 +36,7 @@ def game_hash()
           blocks: 1,
           slam_dunks: 15
         },
-        "Mason Plumlee" => {
-          player_name: "Mason Plumlee",
+        player_name: "Mason Plumlee" => {
           number: 1,
           shoe: 19,
           points: 26,
@@ -50,8 +46,7 @@ def game_hash()
           blocks: 8,
           slam_dunks: 5
         },
-        "Jason Terry" => {
-          player_name: "Jason Terry",
+        player_name: "Jason Terry" => {
           number: 31,
           shoe: 15,
           points: 19,
@@ -67,8 +62,7 @@ def game_hash()
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: [
-        "Jeff Adrien" => {
-          player_name: "Jeff Adrien",
+        player_name: "Jeff Adrien" => {
           number: 4,
           shoe: 18,
           points: 10,
@@ -78,8 +72,7 @@ def game_hash()
           blocks: 7,
           slam_dunks: 2
         },
-        "Bismack Biyombo" => {
-          player_name: "Bismack Biyombo",
+        player_name: "Bismack Biyombo" => {
           number: 0,
           shoe: 16,
           points: 12,
@@ -89,8 +82,7 @@ def game_hash()
           blocks: 15,
           slam_dunks: 10
         },
-        "DeSagna Diop" => {
-          player_name: "DeSagna Diop",
+        player_name: "DeSagna Diop" => {
           number: 2,
           shoe: 14,
           points: 24,
@@ -100,8 +92,7 @@ def game_hash()
           blocks: 5,
           slam_dunks: 5
         },
-        "Ben Gordon" => {
-          player_name: "Ben Gordon",
+        player_name: "Ben Gordon" => {
           number: 8,
           shoe: 15,
           points: 33,
